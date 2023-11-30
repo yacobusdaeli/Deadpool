6f230fd7222efa2315aee915793e8b6170a471cd
raghnat <rayhan.aghnat@mhs.unsoed.ac.id>
2023-12-01 01:41:23 +0700

    Masang login dan register

---
+++ homepage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+</head>
+
+<body>
+
+    <div class="overlay-container">
+        <img class="background-image" src="assets/css/background.jpg" alt="Background Image">
+        <div class="overlay"></div>
+        <div class="content-img">
+            <!-- Your content goes here -->
+            <img class="overlay-image" src="assets/css/home.png" alt="Overlay Image">
+        </div>
+        <div class="slicing"></div>
+    </div>
+    <!-- Navbar section -->
+    <div class="navbar-section">
+        <div class="navbar">
+            <div class="logo-container">
+                <img class="logo" src="assets/css/logo.png" alt="Logo">
+            </div>
+            <ul class="nav-links">
+                <li><a href="home.html">Home</a></li>
+                <li><a href="cast.html">Cast</a></li>
+                <li><a href="achievement.html">Achievement</a></li>
+                <li><a href="synopsis.html">Synopsis</a></li>
+
+                <!-- Add more navigation links as needed -->
+            </ul>
+        </div>
+    </div>
+    <!-- End navbar section -->
+
+    <!-- Start description -->
+    <div class="container-desc">
+        <div class="main">
+            <h1>Hi, Im Deadpool !</h1>
+            <p>Deadpool adalah sebuah film pahlawan super Amerika yang berdasarkan pada karakter Marvel Comics dengan
+                nama yang sama. Film tersebut merupakan instalmen kedelapan dalam serial film X-Men</p>
+        </div>
+        <div class="container-button">
+
+            <button type="submit" class="button-trailer">
+                <a href="synopsis.html">
+                    <i class="fa-solid fa-play fa-lg" style="color: #ffffff;"></i>
+                    <span>Read More</span>
+                </a>
+        </div>
+    </div>
+
+    <!-- End Description  -->
+
+    <!-- Start footer  -->
+    <div class="footer">
+        <p> © 2023 Deadpool Project</p>
+    </div>
+    <!-- end footer  -->
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,66 @@
+<?php
+require 'method.php';
+session_start();
+
+if (isset($_POST['register'])) {
+    if (register($_POST) > 0) {
+        echo "
+        <script>
+        alert('akun berhasil di buat');
+        document.location.href = 'login.php';
+        </script>
+        ";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Quicksand:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <title>Tampilan Register</title>
+</head>
+<div class="logo-image-register">
+    <img src="/assets/css/register-image.png" alt="Deadpool Smiley">
+
+    <div class="login-container">
+        <div class="login-header">
+            <h1>Sign Up</h1>
+        </div>
+
+        <form>
+            <div class="form-group">
+                <label for="username"></label>
+                <input type="username" id="username" name="username" placeholder="Username" required>
+            </div>
+            <div class="form-group">
+                <label for="email"></label>
+                <input type="email" id="email" name="email" placeholder="Email" required>
+            </div>
+            <div class="form-group">
+                <label for="password"></label>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+            </div>
+
+            <div class="sign-up-link">
+                <p>Already have an accout ? <a href="register.html">Login</a></p>
+            </div>
+
+            <button type="submit" class="login-button">Login</button>
+        </form>
+    </div>
+</div>
+
+<body>
+
+</body>
+
+</html>
